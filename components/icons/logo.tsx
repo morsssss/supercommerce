16f241805eb9878c2233a0541af8b3e0d32d5d10
Ben Morss <morss@google.com>
2024-08-08 22:41:14 -0400

    New logo image, bits of customized text

--- components/icons/logo.tsx
+++ components/icons/logo.tsx
@@ -1,16 +1,12 @@
-import clsx from 'clsx';
+import Image from 'next/image';
 
-export default function LogoIcon(props: React.ComponentProps<'svg'>) {
+export default function LogoIcon() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      aria-label={`${process.env.SITE_NAME} logo`}
-      viewBox="0 0 32 28"
-      {...props}
-      className={clsx('h-4 w-4 fill-black dark:fill-white', props.className)}
-    >
-      <path d="M21.5758 9.75769L16 0L0 28H11.6255L21.5758 9.75769Z" />
-      <path d="M26.2381 17.9167L20.7382 28H32L26.2381 17.9167Z" />
-    </svg>
+    <Image
+      src="/disco_head_40x40.png"
+      width={40}
+      height={40}
+      alt="Tiny disco boi"
+    />
   );
 }
