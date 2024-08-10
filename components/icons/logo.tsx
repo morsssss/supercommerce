b59a6f3497b928c836017fb23a276291f1902ef3
Ben Morss <morss@google.com>
2024-08-09 23:14:18 -0400

    Let's try again

--- components/icons/logo.tsx
+++ components/icons/logo.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image';
 
-export default function LogoIcon(props:object) {
+export default function LogoIcon(props:any) {
 
   return (
     <Image
+      className={props.className}
       src="/disco_head_40x40.png"
       alt="Tiny disco boi"
       {...props}
